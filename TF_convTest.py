80d966fddfb4d7013c74120631a63c902f80a157
lmolhw <4292132472@qq.com>
2016-12-02 11:36:36 +0800

    TF_convTest

--- TF_convTest.py
+++ TF_convTest.py
@@ -36,7 +36,6 @@ def max_pool_2x2(x):
 #最后是输出的通道数目，而对于每一个输出通道都由一个对应的偏置量
 W_conv1 = weight_variable([5,5,1,32])
 b_conv1 = bias_variable([32])
-# print(W_conv1.value(),b_conv1.value())
 
 
 #把x变成4维向量2,3维代表宽高，最后一维代表图片的颜色通道数，rgb=3，灰度图=1
@@ -61,7 +60,9 @@ h_pool2 = max_pool_2x2(h_conv2)
 W_fc1 = weight_variable([7*7*64,1024])
 b_fc1 = bias_variable([1024])
 
+#reshape pooling后的数据，把数据输出成7*7*64大小的向量
 h_pool2_flat = tf.reshape(h_pool2,[-1,7*7*64])
+#在全连接层，直接向量内积
 h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat,W_fc1)+b_fc1)
 
 #为了减少过拟合，在输出层之前加入dropout
@@ -72,23 +73,34 @@ h_fc1_drop = tf.nn.dropout(h_fc1,keep_prob)
 W_fc2 = weight_variable([1024,10])
 b_fc2 = bias_variable([10])
 
+#(1*1024)*(1024*10)=(1*10)
 y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop,W_fc2)+b_fc2)
+
+
 
 #测评
 
+
 #交叉熵
 cross_entropy = -tf.reduce_sum(y_*tf.log(y_conv))
-#步长
-train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 
+#步长
+#Adam随机目标函数算法
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+#train_step = tf.train.GradientDescentOptimizer(1e-3).minimize(cross_entropy)
+
+#检测是否实现标签匹配
 correct_prediction = tf.equal(tf.argmax(y_conv,1),tf.argmax(y_,1))
 
 accuracy = tf.reduce_mean(tf.cast(correct_prediction,"float"))
-
+#初始化所有变量
 sess.run(tf.initialize_all_variables())
+
 for i in range(2000):
     batch = mnist.train.next_batch(50)
+    # print(batch)
     if i%100 ==0:
+        #通过给eval或者run函数输入feed_dict参数，可以启动运算过程
         train_accuracy = accuracy.eval(feed_dict={
             x:batch[0],y_:batch[1],keep_prob:1.0
         })
@@ -96,5 +108,4 @@ for i in range(2000):
     train_step.run(feed_dict={x:batch[0],y_:batch[1],keep_prob:0.5})
 
 print("test accuracy %g"%accuracy.eval(feed_dict={
-    x:mnist.test.images,y_:mnist.test.labels,keep_prob:1.0
-}))
+    x:mnist.test.images,y_:mnist.test.labels,keep_prob:1.0}))
