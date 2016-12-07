3c6614fe2cf8f8c5e32b658562648dc3610da0f0
lmolhw <4292132472@qq.com>
2016-12-07 13:36:06 +0800

    Merge branch 'master' of /home/lmolhw/PycharmProjects/demo with conflicts.

--- Recognizing_hand-written_digits.py
+++ Recognizing_hand-written_digits.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 # print(__doc__)
 
 # Author: Gael Varoquaux <gael dot varoquaux at normalesup dot org>
@@ -7,16 +8,24 @@
 import matplotlib.pyplot as plt
 
 
+=======
+import matplotlib.pyplot as plt
+
+>>>>>>> Stashed changes
 # Import datasets, classifiers and performance metrics
 from sklearn import datasets, svm, metrics
 
 # The digits dataset
 digits = datasets.load_digits()
+<<<<<<< Updated upstream
 # print(digits.data[0])
 # for i in range(10):
 #     plt.imshow(digits.images[i], cmap=plt.cm.gray_r, interpolation='nearest')
 #     plt.title('digits.target[%s]' %i)
 #     plt.show()
+=======
+
+>>>>>>> Stashed changes
 # The data that we are interested in is made of 8x8 images of digits, let's
 # have a look at the first 4 images, stored in the `images` attribute of the
 # dataset.  If we were working from image files, we could load them using
@@ -26,6 +35,7 @@ digits = datasets.load_digits()
 
 images_and_labels = list(zip(digits.images, digits.target))
 
+<<<<<<< Updated upstream
 # print(images_and_labels)
 
 for index, (image, label) in enumerate(images_and_labels[:4]):
@@ -40,19 +50,31 @@ for index, (image, label) in enumerate(images_and_labels[:4]):
 
     plt.title('Training: %i' % label)
     # plt.show()
+=======
+for index, (image, label) in enumerate(images_and_labels[:4]):
+    plt.subplot(2, 4, index + 1)
+    plt.axis('off')
+    plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
+    plt.title('Training: %i' % label)
+>>>>>>> Stashed changes
 
 # To apply a classifier on this data, we need to flatten the image, to
 # turn the data in a (samples, feature) matrix:
 n_samples = len(digits.images)
+<<<<<<< Updated upstream
 # print(n_samples)
 
 data = digits.images.reshape((n_samples, -1))
 # print(data)
+=======
+data = digits.images.reshape((n_samples, -1))
+>>>>>>> Stashed changes
 
 # Create a classifier: a support vector classifier
 classifier = svm.SVC(gamma=0.001)
 
 # We learn the digits on the first half of the digits
+<<<<<<< Updated upstream
 # 使用前一半进行训练
 classifier.fit(data[:n_samples / 2], digits.target[:n_samples / 2])
 
@@ -60,15 +82,25 @@ classifier.fit(data[:n_samples / 2], digits.target[:n_samples / 2])
 # 用后一半进行测试
 expected = digits.target[n_samples / 2:]
 
+=======
+classifier.fit(data[:n_samples / 2], digits.target[:n_samples / 2])
+
+# Now predict the value of the digit on the second half:
+expected = digits.target[n_samples / 2:]
+>>>>>>> Stashed changes
 predicted = classifier.predict(data[n_samples / 2:])
 
 print("Classification report for classifier %s:\n%s\n"
       % (classifier, metrics.classification_report(expected, predicted)))
 print("Confusion matrix:\n%s" % metrics.confusion_matrix(expected, predicted))
 
+<<<<<<< Updated upstream
 
 images_and_predictions = list(zip(digits.images[n_samples / 2:], predicted))
 
+=======
+images_and_predictions = list(zip(digits.images[n_samples / 2:], predicted))
+>>>>>>> Stashed changes
 for index, (image, prediction) in enumerate(images_and_predictions[:4]):
     plt.subplot(2, 4, index + 5)
     plt.axis('off')
