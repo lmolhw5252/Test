8fd730fcad8fbac16150b7d6a8a606f03ff5d257
lmolhw <4292132472@qq.com>
2016-12-08 11:48:35 +0800

    hand_written 8x8的识别效果太低

--- IMG_resize.py
+++ IMG_resize.py
@@ -2,10 +2,14 @@ from PIL import Image
 import  numpy as np
 import matplotlib.pyplot as plt
 
-pil_im = Image.open('00004.png').convert('L')
+pil_im = Image.open('1.png').convert('L')
+plt.imshow(pil_im,cmap=plt.cm.gray_r, interpolation='nearest')
+plt.show()
 
-out = np.array(pil_im.resize((64,64)))
+out = np.array(pil_im.resize((8,8)))
 
 plt.imshow(out,cmap=plt.cm.gray_r, interpolation='nearest')
 plt.show()
-print(out.__class__)
+print(out.shape)
+out = np.reshape(out,(1,-1))
+print(out.shape)
--- Recognizing_hand-written_digits.py
+++ Recognizing_hand-written_digits.py
@@ -12,14 +12,15 @@ from sklearn import datasets, svm, metrics
 
 # The digits dataset
 digits = datasets.load_digits()
-
-# print(digits.data[0])
+#1797张8x8的图
+# print(digits.images.shape)
 
 #显示每张图
 # for i in range(10):
 #     plt.imshow(digits.images[i], cmap=plt.cm.gray_r, interpolation='nearest')
 #     plt.title('digits.target[%s]' %i)
 #     plt.show()
+
 
 # The data that we are interested in is made of 8x8 images of digits, let's
 # have a look at the first 4 images, stored in the `images` attribute of the
@@ -28,71 +29,89 @@ digits = datasets.load_digits()
 # images, we know which digit they represent: it is given in the 'target' of
 # the dataset.
 
+#把图像数组和target放在一起
 images_and_labels = list(zip(digits.images, digits.target))
 
-# print(images_and_labels)
+# print(images_and_labels[0])
 
-for index, (image, label) in enumerate(images_and_labels[:4]):
-    #创建一个两行四列的图，图的数量为index+1
-    plt.subplot(2, 4, index + 1)
-    #设置轴属性,turns off the axis lines and labels
-    plt.axis('off')
-    #在axes上显示图像
-    #X : array_like, shape (n, m) or (n, m, 3) or (n, m, 4)
-    #camp,如果为None，则忽略图像的RGB值
-    plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
-
-    plt.title('Training: %i' % label)
-    # plt.show()
+# for index, (image, label) in enumerate(images_and_labels[:4]):
+#     #创建一个两行四列的图，图的数量为index+1
+#     plt.subplot(2, 4, index + 1)
+#     #设置轴属性,忽略横轴
+#     plt.axis('off')
+#     #在axes上显示图像
+#     #X : array_like, shape (n, m) or (n, m, 3) or (n, m, 4)
+#     #camp,如果为None，则忽略图像的RGB值
+#     plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
+#
+#     plt.title('Training: %i' % label)
+# plt.show()
 
 # To apply a classifier on this data, we need to flatten the image, to
 # turn the data in a (samples, feature) matrix:
-n_samples = len(digits.images)
-print(n_samples)
 
+n_samples = len(digits.images)
+# print("n_samples",n_samples)
+
+#把图像变成1797x64的矩阵
 data = digits.images.reshape((n_samples, -1))
-# print(data)
+# print(data.shape)
 
 # Create a classifier: a support vector classifier
 classifier = svm.SVC(gamma=0.001)
 
 # We learn the digits on the first half of the digits
-# 使用前一半进行训练
+'''使用前一半进行训练'''
 classifier.fit(data[:n_samples / 2], digits.target[:n_samples / 2])
 
 # Now predict the value of the digit on the second half:
 # 用后一半进行测试
 expected = digits.target[n_samples / 2:]
+# print(data[n_samples / 2:].shape)
 
 predicted = classifier.predict(data[n_samples / 2:])
 
+'''全部作为训练集'''
+# classifier.fit(data,digits.target)
+# print(predicted.shape)
+
 print("Classification report for classifier %s:\n%s\n"
       % (classifier, metrics.classification_report(expected, predicted)))
-
+#
 print("Confusion matrix:\n%s" % metrics.confusion_matrix(expected, predicted))
 
+# print(predicted.__class__)
 
+# print(digits.images[n_samples / 2:].shape)
 images_and_predictions = list(zip(digits.images[n_samples / 2:], predicted))
 
-for index, (image, prediction) in enumerate(images_and_predictions[:4]):
+# print(digits.images[n_samples / 2:].shape)
+
+for index, (image, prediction) in enumerate(images_and_predictions[5:9]):
+    # print(prediction)
     plt.subplot(2, 4, index + 5)
     plt.axis('off')
     plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
+    # print(image.shape)
     plt.title('Prediction: %i' % prediction)
 
 plt.show()
+'''输入图片测试'''
+pil_im = Image.open('9.png').convert('L')
 
-pil_im = Image.open('00004.png').convert('L')
+out = np.array(pil_im.resize((8,8)))
+# print(out.shape)
 
-out = np.array(pil_im.resize((64,64)))
+out_test = np.reshape(out,(1,-1))
 
-out.reshape(-1,64)
-print(out.shape)
-print(out)
+# print(out_test.shape)
 
-plt.imshow(out,cmap=plt.cm.gray_r, interpolation='nearest')
+
+test  = classifier.predict(out_test)
+
+plt.imshow(out, cmap=plt.cm.gray_r, interpolation='nearest')
+plt.title('Prediction: %i' %test)
 plt.show()
 
-test  = classifier.predict(out)
 
-print(test)
+# print(test.__class__)
